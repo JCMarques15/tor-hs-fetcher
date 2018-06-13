37022229c7289fda54356336305fa9610ba1a260
JCMarques15 <jcmarques.jcm@gmail.com>
2018-06-13 17:52:15 +0200

    HS_DESC_CONTENT can have a lot of introduction point so i decided to just diplay number of introduction points

--- hs_fecther.py
+++ hs_fecther.py
@@ -39,7 +39,7 @@ def hs_desc_handler(event):
         else:
             parsed_descriptor = stem.descriptor.hidden_service_descriptor.HiddenServiceDescriptor(descriptor_text, validate=True)
             introduction_points = parsed_descriptor.introduction_points()
-            logger.info("Descriptor contains:\n\tAdress: {}\n\tDirectory: {}\n\tDescriptor_id: {}\n\tIntroduction_Points: {}".format(event.address, event.directory, event.descriptor_id, introduction_points))
+            logger.info("Descriptor contains:\n\tAdress: {}\n\tDirectory: {}\n\tDescriptor_id: {}\n\tNum_Introduction_Points: {}".format(event.address, event.directory, event.descriptor_id, len(introduction_points)))
 
 
 
