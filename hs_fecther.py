86f365e6186a52c4ab365d400cdbf36e24eaa1e0
JCMarques15 <jcmarques.jcm@gmail.com>
2018-06-13 16:43:56 +0200

    added HS_DESC_CONTENT event capture

--- hs_fecther.py
+++ hs_fecther.py
@@ -7,6 +7,7 @@ import datetime
 import os
 import logging
 import errno
+
 import stem
 import stem.control
 
@@ -19,15 +20,31 @@ logger.setLevel(logging.DEBUG)
 
 
 def hs_desc_handler(event):
-  if event.type == "HS_DESC":
-    logger.info("HS_DESC received")
-    if event.reason:
-      logger.info("Descriptor fetching from {} for HS {} failed with error: {}".format(event.directory_fingerprint, event.address, event.reason))
-    else:
-      logger.info("Descriptor contains:\n\tAdress: {}\n\tAuthentication: {}\n\tDirectory: {}\n\tDescriptor_id: {}".format(event.address, event.authentication, event.directory, event.descriptor_id))
+    if event.type == "HS_DESC":
+        logger.info("HS_DESC received")
+        if event.reason:
+            logger.info("Descriptor fetching from {} for HS {} failed with error: {}".format(event.directory_fingerprint, event.address, event.reason))
+        else:
+            logger.info("Descriptor contains:\n\tAdress: {}\n\tAuthentication: {}\n\tDirectory: {}\n\tDescriptor_id: {}".format(event.address, event.authentication, event.directory, event.descriptor_id))
+
+    if event.type == "HS_DESC_CONTENT":
+        logger.info("HS_DESC_CONTENT received")
+        # Save the descriptor content to disk
+        descriptor_text = str(event.descriptor).encode('utf-8')
+
+        # Make sure the descriptor is not empty
+        if len(descriptor_text) < 5:
+            logger.debug("Empty descriptor received for %s" % event.address)
+            return
+        else:
+            parsed_descriptor = stem.descriptor.hidden_service_descriptor.HiddenServiceDescriptor(descriptor_text, validate=True)
+            introduction_points = parsed_descriptor.introduction_points()
+            logger.info("Descriptor contains:\n\tAdress: {}\n\tAuthentication: {}\n\tDirectory: {}\n\tDescriptor_id: {}\n\tIntroduction_Points: {}".format(event.address, event.authentication, event.directory, event.descriptor_id, introduction_points))
+
+
 
 def main():
-  with stem.control.Controller.from_port(port=9051) as controller:
+    with stem.control.Controller.from_port(port=9051) as controller:
         # Create a connection to the Tor control port
         try:
             controller.authenticate()
@@ -42,12 +59,12 @@ def main():
         controller.add_event_listener(hs_desc_handler, stem.control.EventType.HS_DESC)
         controller.add_event_listener(hs_desc_handler, stem.control.EventType.HS_DESC_CONTENT)
 
-  try:
-    while True:
-        continue
-  except KeyboardInterrupt:
-    logger.info("Stopping descriptor fetching")
-    sys.exit(0)
+    try:
+        while True:
+            continue
+    except KeyboardInterrupt:
+        logger.info("Stopping descriptor fetching")
+        sys.exit(0)
 
 if __name__ == '__main__':
     main()
