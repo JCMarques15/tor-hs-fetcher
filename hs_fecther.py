01acfbd6be0b1a98b79006885efa955bba7c283a
JCMarques15 <jcmarques.jcm@gmail.com>
2018-06-13 15:26:46 +0200

    Completed the first run of the script, not to testing

--- hs_fecther.py
+++ hs_fecther.py
@@ -1,13 +1,17 @@
+import sys
+import time
+import argparse
+import struct
+import hashlib
+import datetime
+import os
 import logging
-
+import errno
 import stem
 import stem.control
-import schedule
-
 
 handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter(fmt="%(asctime)s [%(levelname)s]: "
-                                           "%(message)s"))
+handler.setFormatter(logging.Formatter(fmt="%(asctime)s [%(levelname)s]: %(message)s"))
 
 logger = logging.getLogger("onion-load-balancer")
 logger.addHandler(handler)
@@ -16,9 +20,34 @@ logger.setLevel(logging.DEBUG)
 
 def hs_desc_handler(event):
   if event.type == "HS_DESC":
-    logger.info("")
+    logger.info("HS_DESC received")
     if event.reason:
-      logger.info("Descriptor fetching from {} for HS {} failed "
-                  "with error: {}".format(event.directory_fingerprint, event.address, event.reason))
+      logger.info("Descriptor fetching from {} for HS {} failed with error: {}".format(event.directory_fingerprint, event.address, event.reason))
     else:
-      logger.info("Received 
+      logger.info("Descriptor contains:\n\tAdress: {}\n\tAuthentication: {}\n\tDirectory: {}\n\tDescriptor_id: {}".format(event.address, event.authentication, event.directory, event.descriptor_id))
+
+def main():
+  with stem.control.Controller.from_port("9051") as controller:
+        # Create a connection to the Tor control port
+        try:
+            controller.authenticate()
+        except stem.connection.AuthenticationFailure as exc:
+            logger.error("Unable to authenticate to Tor control port: %s" % exc)
+            sys.exit(1)
+        else:
+            controller.set_caching(False)
+            logger.debug("Successfully connected to the Tor control port")
+
+        # Add event listeners for HS_DESC and HS_DESC_CONTENT
+        controller.add_event_listener(hs_desc_handler, stem.response.events.HSDescEvent)
+        controller.add_event_listener(hs_desc_handler, stem.response.events.HSDescContentEvent)
+
+  try:
+    while True:
+        continue
+  except KeyboardInterrupt:
+    logger.info("Stopping descriptor fetching")
+    sys.exit(0)
+
+if __name__ == '__main__':
+    main()
