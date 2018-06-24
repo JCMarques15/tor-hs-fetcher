9bf18edf6d162d69ea1330d23ee886740bda5aec
JCMarques15 <jcmarques.jcm@gmail.com>
2018-06-24 10:18:10 +0200

    No idea anymore what i did...

--- hs_fecther.py
+++ hs_fecther.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 import subprocess
-import sys
+import subprocess
+import sys 
 import threading
 import sqlite3
 import re
@@ -10,18 +11,31 @@ import datetime
 class myThread (threading.Thread):
   def __init__(self, threadID, name, pid, db, lock):
     threading.Thread.__init__(self)
+    # Initialize variables passed on object creation
     self.threadID = threadID
     self.name = name
     self.pid = pid
     self.db = db
     self.cursor = db.cursor()
     self.lock = lock
+    
+    # Initalize variable with formated date for later directory naming
     self.extraction_datetime = datetime.datetime.today().strftime('%Y-%m-%d-%H')
+
+    # Initialize regex processing rules
     self.full_descriptor_regex = re.compile("rendezvous-service-descriptor.*?-----END SIGNATURE-----", re.DOTALL)
-    self. = re.compile("", re.DOTALL)
-    self. = re.compile("", re.DOTALL)
+    self.rendezvous_regex = re.compile("rendezvous-service-descriptor\s(.*)")
+    self.descriptor_version_regex = re.compile("version\s(.*)")
     self.descriptor_pkey_regex = re.compile("permanent-key\n-----BEGIN RSA PUBLIC KEY-----(.*?)-----END RSA PUBLIC KEY-----", re.DOTALL)
-    self. = re.compile("", re.DOTALL)
+    self.secret_id_regex = re.compile("secret-id-part\s(.*)")
+    self.publication_time_regex = re.compile("publication-time\s(.*)")
+    self.protocol_versions_regex = re.compile("protocol-versions\s(.*)")
+    self.introduction_points_encoded_regex = re.compile("introduction-points\n-----BEGIN MESSAGE-----(.*?)-----END MESSAGE-----", re.DOTALL)
+    self.signature_regex = re.compile("signature\n-----BEGIN SIGNATURE-----(.*?)-----END SIGNATURE-----", re.DOTALL)
+
+    # Initialize regez to process decoded introduction points
+    self.full_introduction_points_decoded_regex = re.compile("introduction-point.*?-----END RSA PUBLIC KEY-----.*?-----END RSA PUBLIC KEY", re.DOTALL)
+
 
   def run(self):
     print ("Starting {} with pid: {}".format(self.name, self.pid))
@@ -31,28 +45,40 @@ class myThread (threading.Thread):
     for self.line in self.script_output:
       print(self.line)
 
+    # Reads the contents of the strings file into a variable
     with open("{}/Memory_Dumps/{}H-{}".format(sys.path[0], self.extraction_datetime, self.pid), "r") as self.strings_file:
       self.file_contents = self.strings_file.read()
     
+    # Takes all of the descriptors out of the strings variable and process each one by one
     for self.descriptor in self.full_descriptor_regex.match(self.file_contents):
+      # Extracts each field into his own variable
+      self.rendezvous = self.rendezvous_regex.match(self.descriptor).group(1)
+      self.descriptor_version = self.descriptor_version_regex.match(self.descriptor).group(1)
       self.pkey = self.descriptor_pkey_regex.match(self.descriptor).group(1)
+      self.secret_id = self.secret_id_regex.match(self.descriptor).group(1)
+      self.publication_time = self.publication_time_regex.match(self.descriptor).group(1)
+      self.protocol_versions = self.protocol_versions_regex.match(self.descriptor).group(1)
+      self.introduction_points_encoded = self.introduction_points_encoded_regex.match(self.descriptor).group(1)
+      self.signature = self.signature_regex.match(self.descriptor).group(1)
       self.onion_link = "{}.onion".format(self.calc_onion_link(self.pkey))
+
+      # Extracts each introduction point and adds it to a list
+      self.introduction_points_list = self.full_introduction_points_decoded_regex.match(self.decode_introduction_points(self.introduction_points_encoded))
 
       with self.lock.acquire():
         print("{}: Aquired lock".format(self.name))
         self.cursor.execute("INSERT INTO hidden_services(link) VALUES(?)", (self.onion_link,))
         self.onion_link_id = self.cursor.execute("SELECT id FROM hidden_services WHERE link=?", (self.onion_link,))
-        self.cursor.execute("INSERT INTO descriptors(link_id, rendezvous_service_descriptor, descriptor_id, format_version, permanent_key, secret_id_part, publication_time, protocol_versions, descriptor_signature) VALUES(:link_id, :rendezvous, :descriptor_id, :format_version, :permanent_key, :secret_id_part, :publication_time, :protocol_versions, :descriptor_signature)", {
+        self.cursor.execute("INSERT INTO descriptors(link_id, rendezvous_service_descriptor, descriptor_id, format_version, permanent_key, secret_id_part, publication_time, protocol_versions, descriptor_signature) VALUES(:link_id, :rendezvous, :descriptor_id, :format_version, :permanent_key, :secret_id, :publication_time, :protocol_versions, :descriptor_signature)", {
           "link_id":self.onion_link_id, 
-          "rendezvous":, 
-          "descriptor_id":, 
-          "format_version":, 
-          "permanent_key":, 
-          "secret_id_part":, 
-          "publication_time":, 
-          "protocol_versions":, 
-          "descriptor_signature":})
-        for self.entry in self.introduction_points:
+          "rendezvous":self.rendezvous,
+          "format_version":self.descriptor_version, 
+          "permanent_key":self.pkey, 
+          "secret_id":self.secret_id, 
+          "publication_time":self.publication_time, 
+          "protocol_versions":self.protocol_versions, 
+          "descriptor_signature":self.signature})
+        for self.entry in self.introduction_points_list:
           self.cursor.execute("INSERT INTO 
         self.db.commit()
       #self.lock.release()
@@ -70,7 +96,11 @@ class myThread (threading.Thread):
     self.output, self._err = self.process_manager.communicate()
     return self.output.splitlines()[0]
 
-  def decode_introduction_points
+  def decode_introduction_points(self, encoded_introduction_points):
+    self.process_manager_echo = subprocess.Popen(["echo", encoded_introduction_points], stdout=subprocess.PIPE)
+    self.process_manager_base64 = subprocess.Popen(["base64", "-d"], stdin=self.process_manager_echo.stdout, stdout=subprocess.PIPE, universal_newlines=True)
+    self.output, self._err = self.process_manager_base64.communicate()
+    return self.output
 
 
 def extract_pid():
