83031dee6969d3de81ff044620133d363ce7bdb4
JCMarques15 <jcmarques.jcm@gmail.com>
2018-06-28 16:19:46 +0200

    made some changes to the sh script

--- sql_queries.sh
+++ sql_queries.sh
@@ -12,7 +12,7 @@ while true; do
     [4]* ) sqlite3 hidden_services.db "select * from descriptors_snapshot" | awk -F"|" '{for(c=0;c<82;c++) printf "-"; print "\nID\tLink_ID\t\tDescriptor_Fingerprint\t\t\tDescriptor_Version"; print $1"\t"$2"\t\t"$3"\t"$4; print "\nPublic-Key\n"$5; print "\nSecret-ID\t\t\t\tPublication_time\tProtocol_Version"; print $6"\t"$7"\t"$8; print "\nIntroduction_points\n"$9; print "\nSignature\n"$10;}' | less; clear;;
     [5]* ) sqlite3 hidden_services.db "select * from v3_descriptors" | awk -F"|" 'BEGIN{print "ID\tCERT"} {print $1"\t"$2}' | less; clear;;
     [6]* ) sqlite3 hidden_services.db "select * from extraction_stats;" | awk -F"|" 'BEGIN{print "V3\tV2\tDATE\t\tProcess"} {print $1"\t"$2"\t"$3"\t"$4}' | less; clear;;
-    [Qq]* ) exit(0);;
+    [Qq]* ) exit;;
     * ) clear; echo "Uknown option.";;
   esac
 done
